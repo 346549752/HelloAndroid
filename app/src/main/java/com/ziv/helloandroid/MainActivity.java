85c112e0add7a9d9966b584801ec6e21efe21ece
Ziv <346549752@qq.com>
2015-07-24 15:47:05 +0800

    添加hello注释

--- app/src/main/java/com/ziv/helloandroid/MainActivity.java
+++ app/src/main/java/com/ziv/helloandroid/MainActivity.java
@@ -3,16 +3,13 @@ package com.ziv.helloandroid;
 import android.app.Activity;
 import android.os.Bundle;
 
+
 public class MainActivity extends Activity {
-    //This is a test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
     }
-
+    //hello world
 }
-
